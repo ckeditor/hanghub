bd0a918e99cc64743748b3af9ecd37ac31c93b78
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-06-24 10:51:08 +0200

    Improve test suites description. Simplify tests a bit.

--- backend/tests/helpers/UserHelper.test.js
+++ backend/tests/helpers/UserHelper.test.js
@@ -1,61 +1,58 @@
 const { expect } = require( 'chai' );
 const { getUserListFromSessions } = require( '../../src/helpers/helpers' );
 
-const nowTimestamp = Date.now();
-const pastTimeTimestamp = Date.now() - 10000;
-
-const sessionsData = {
-	IbVmSiD_LoULFK2yAAAB: {
-		id: '123',
-		state: 'commenting',
-		joinedAt: nowTimestamp
-	},
-	IbVmSiD_LoUPLNYAAAB: {
-		id: '123',
-		state: 'editing',
-		joinedAt: nowTimestamp
-	},
-	Boc1d1GISKfDnD5zAAA: {
-		id: '123',
-		state: 'away',
-		joinedAt: nowTimestamp
-	},
-	Boc1GUHISK321D5zAAAQ: {
-		id: '123',
-		state: 'viewing',
-		joinedAt: nowTimestamp
-	},
-	Boc1GUHIS423DZNSQERQ: {
-		id: '1234',
-		state: 'viewing',
-		joinedAt: pastTimeTimestamp
-	}
-};
-
-const singleSessionData = {
-	IbVmSiD_LoULFK2yAAAB: {
-		id: '123',
-		state: 'commenting',
-	},
-};
-
 describe( 'getUsers', function() {
-	it( 'Should return sorted list of users', function() {
+	it( 'should return a sorted ascending list of connected users', function() {
+		const sessionsData = {
+			IbVmSiD_LoULFK2yAAAB: {
+				id: '123',
+				state: 'commenting',
+				joinedAt: 1561365746095
+			},
+			IbVmSiD_LoUPLNYAAAB: {
+				id: '123',
+				state: 'editing',
+				joinedAt: 1561365746095
+			},
+			Boc1d1GISKfDnD5zAAA: {
+				id: '123',
+				state: 'away',
+				joinedAt: 1561365746095
+			},
+			Boc1GUHISK321D5zAAAQ: {
+				id: '123',
+				state: 'viewing',
+				joinedAt: 1561365746095
+			},
+			Boc1GUHIS423DZNSQERQ: {
+				id: '1234',
+				state: 'viewing',
+				joinedAt: 1561365745095
+			}
+		};
+
 		expect( getUserListFromSessions( sessionsData ) ).to.deep.equal( [
 			{
 				id: '123',
 				state: 'editing',
-				joinedAt: nowTimestamp
+				joinedAt: 1561365746095
 			},
 			{
 				id: '1234',
 				state: 'viewing',
-				joinedAt: pastTimeTimestamp
+				joinedAt: 1561365745095
 			},
 		] );
 	} );
 
-	it( 'Should return single user', function() {
+	it( 'should return a single connected user when only one user is provided', function() {
+		const singleSessionData = {
+			IbVmSiD_LoULFK2yAAAB: {
+				id: '123',
+				state: 'commenting',
+			},
+		};
+
 		expect( getUserListFromSessions( singleSessionData ) ).to.deep.equal( [
 			{
 				id: '123',
@@ -63,7 +60,8 @@ describe( 'getUsers', function() {
 			}
 		] );
 	} );
-	it( 'Should return empty array', function() {
+
+	it( 'should return an empty list of connected users when there are no users provided', function() {
 		expect( getUserListFromSessions( {} ) ).to.deep.equal( [] );
 	} );
 } );
