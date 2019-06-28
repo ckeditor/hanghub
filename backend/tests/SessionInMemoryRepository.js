51a730ffa8301c1acfec068a466bba3eae1862fa
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-06-28 15:39:47 +0200

    Improve tests. Add tests for set() and delete() methods.

--- backend/tests/SessionInMemoryRepository.js
+++ backend/tests/SessionInMemoryRepository.js
@@ -1,7 +1,7 @@
 const { expect } = require( 'chai' );
 const SessionInMemoryRepository = require( '../src/SessionInMemoryRepository' );
 
-describe( 'getAll() in-memory implementation', () => {
+describe( 'SessionInMemoryRepository', () => {
 	const issueKey = 'test/repository:issue/1';
 	let repository;
 
@@ -9,68 +9,90 @@ describe( 'getAll() in-memory implementation', () => {
 		repository = new SessionInMemoryRepository();
 	} );
 
-	it( 'should return a sorted ascending list of connected users', () => {
-		const sessionsData = {
-			IbVmSiD_LoULFK2yAAAB: {
-				id: '123',
-				state: 'commenting',
-				joinedAt: 1561365746095
-			},
-			IbVmSiD_LoUPLNYAAAB: {
-				id: '123',
-				state: 'editing',
-				joinedAt: 1561365746095
-			},
-			Boc1d1GISKfDnD5zAAA: {
-				id: '123',
-				state: 'away',
-				joinedAt: 1561365746095
-			},
-			Boc1GUHISK321D5zAAAQ: {
-				id: '123',
-				state: 'viewing',
-				joinedAt: 1561365746095
-			},
-			Boc1GUHIS423DZNSQERQ: {
-				id: '1234',
-				state: 'viewing',
-				joinedAt: 1561365745095
+	describe( 'getAll()', () => {
+		it( 'should return a sorted ascending list of connected users', () => {
+			const sessionsData = {
+				IbVmSiD_LoULFK2yAAAB: {
+					id: '123',
+					state: 'commenting',
+					joinedAt: 1561365746095
+				},
+				IbVmSiD_LoUPLNYAAAB: {
+					id: '123',
+					state: 'editing',
+					joinedAt: 1561365746095
+				},
+				Boc1d1GISKfDnD5zAAA: {
+					id: '123',
+					state: 'away',
+					joinedAt: 1561365746095
+				},
+				Boc1GUHISK321D5zAAAQ: {
+					id: '123',
+					state: 'viewing',
+					joinedAt: 1561365746095
+				},
+				Boc1GUHIS423DZNSQERQ: {
+					id: '1234',
+					state: 'viewing',
+					joinedAt: 1561365745095
+				}
+			};
+
+			for ( const socketId in sessionsData ) {
+				repository.set( issueKey, socketId, sessionsData[ socketId ] );
 			}
-		};
 
-		for ( const socketId in sessionsData ) {
-			repository.set( issueKey, socketId, sessionsData[ socketId ] );
-		}
-
-		expect( repository.getAll( issueKey ) ).to.deep.equal( [
-			{
-				id: '123',
-				state: 'editing',
-				joinedAt: 1561365746095
-			},
-			{
-				id: '1234',
-				state: 'viewing',
-				joinedAt: 1561365745095
-			},
-		] );
-	} );
-
-	it( 'should return a single connected user when only one user is provided', () => {
-		repository.set( issueKey, 'IbVmSiD_LoULFK2yAAAB', {
-			id: '123',
-			state: 'commenting'
+			expect( repository.getAll( issueKey ) ).to.deep.equal( [
+				{
+					id: '123',
+					state: 'editing',
+					joinedAt: 1561365746095
+				},
+				{
+					id: '1234',
+					state: 'viewing',
+					joinedAt: 1561365745095
+				},
+			] );
 		} );
-		expect( repository.getAll( issueKey ) ).to.deep.equal( [
-			{
+
+		it( 'should return a single connected user when only one user is provided', () => {
+			repository.set( issueKey, 'IbVmSiD_LoULFK2yAAAB', {
 				id: '123',
 				state: 'commenting'
-			}
-		] );
+			} );
+			expect( repository.getAll( issueKey ) ).to.deep.equal( [
+				{
+					id: '123',
+					state: 'commenting'
+				}
+			] );
+		} );
+
+		it( 'should return an empty list of connected users when there are no users provided', () => {
+			expect( repository.getAll( issueKey ) ).to.deep.equal( [] );
+		} );
 	} );
 
-	it( 'should return an empty list of connected users when there are no users provided', () => {
-		expect( repository.getAll( issueKey ) ).to.deep.equal( [] );
+	describe( 'delete()', () => {
+		it( 'should return a single connected user after insertion and an empty list of connected users after deletion', () => {
+			repository.set( issueKey, 'IbVmSiD_LoULFK2yAAAB', {
+				id: '123',
+				state: 'commenting'
+			} );
+
+			expect( repository.getAll( issueKey ) ).to.deep.equal( [
+				{
+					id: '123',
+					state: 'commenting'
+				}
+			] );
+
+			repository.delete( issueKey, 'IbVmSiD_LoULFK2yAAAB' );
+
+			expect( repository.getAll( issueKey ) ).to.deep.equal( [] );
+		} );
 	} );
 } );
 
--- backend/tests/SessionRedisRepository.js
+++ backend/tests/SessionRedisRepository.js
@@ -6,79 +6,124 @@ const io = require( 'socket.io' )( http );
 const RedisDriver = require( '../src/RedisDriver' );
 const SessionRedisRepository = require( '../src/SessionRedisRepository' );
 
-describe( 'getAll() Redis implementation', () => {
+describe( 'SessionRedisRepository', () => {
 	const issueKey = 'test/repository:issue/1';
-	let repository;
+	let repository, redisDriver;
 
-	beforeEach( () => {
-		const redisDriver = new RedisDriver( io, 'localhost', '6379' );
+	beforeEach( async () => {
+		redisDriver = new RedisDriver( io, 'localhost', '6379' );
+
 		redisDriver.connect();
-		redisDriver.client.flushall();
+		await redisDriver.client.flushall();
+
 		repository = new SessionRedisRepository( redisDriver.client );
 	} );
 
-	it( 'should return a sorted ascending list of connected users', async () => {
-		const sessionsData = {
-			IbVmSiD_LoULFK2yAAAB: {
-				id: '123',
-				state: 'commenting',
-				joinedAt: 1561365746095
-			},
-			IbVmSiD_LoUPLNYAAAB: {
-				id: '123',
-				state: 'editing',
-				joinedAt: 1561365746095
-			},
-			Boc1d1GISKfDnD5zAAA: {
-				id: '123',
-				state: 'away',
-				joinedAt: 1561365746095
-			},
-			Boc1GUHISK321D5zAAAQ: {
-				id: '123',
-				state: 'viewing',
-				joinedAt: 1561365746095
-			},
-			Boc1GUHIS423DZNSQERQ: {
-				id: '1234',
-				state: 'viewing',
-				joinedAt: 1561365745095
-			}
-		};
-
-		for ( const socketId in sessionsData ) {
-			await repository.set( issueKey, socketId, sessionsData[ socketId ] );
-		}
-
-		expect( await repository.getAll( issueKey ) ).to.deep.equal( [
-			{
-				id: '123',
-				state: 'editing',
-				joinedAt: 1561365746095
-			},
-			{
-				id: '1234',
-				state: 'viewing',
-				joinedAt: 1561365745095
-			},
-		] );
+	afterEach( async () => {
+		await redisDriver.client.disconnect();
 	} );
 
-	it( 'should return a single connected user when only one user is provided', async () => {
-		await repository.set( issueKey, 'IbVmSiD_LoULFK2yAAAB', {
-			id: '123',
-			state: 'commenting'
+	describe( 'getAll() ', () => {
+		it( 'should return a sorted ascending list of connected users', async () => {
+			const sessionsData = {
+				IbVmSiD_LoULFK2yAAAB: {
+					id: '123',
+					state: 'commenting',
+					joinedAt: 1561365746095
+				},
+				IbVmSiD_LoUPLNYAAAB: {
+					id: '123',
+					state: 'editing',
+					joinedAt: 1561365746095
+				},
+				Boc1d1GISKfDnD5zAAA: {
+					id: '123',
+					state: 'away',
+					joinedAt: 1561365746095
+				},
+				Boc1GUHISK321D5zAAAQ: {
+					id: '123',
+					state: 'viewing',
+					joinedAt: 1561365746095
+				},
+				Boc1GUHIS423DZNSQERQ: {
+					id: '1234',
+					state: 'viewing',
+					joinedAt: 1561365745095
+				}
+			};
+
+			for ( const socketId in sessionsData ) {
+				await redisDriver.client.hset( issueKey, socketId, JSON.stringify( sessionsData[ socketId ] ) );
+			}
+
+			expect( await repository.getAll( issueKey ) ).to.deep.equal( [
+				{
+					id: '123',
+					state: 'editing',
+					joinedAt: 1561365746095
+				},
+				{
+					id: '1234',
+					state: 'viewing',
+					joinedAt: 1561365745095
+				},
+			] );
 		} );
-		expect( await repository.getAll( issueKey ) ).to.deep.equal( [
-			{
+
+		it( 'should return a single connected user when only one user is provided', async () => {
+			await redisDriver.client.hset( issueKey, 'IbVmSiD_LoULFK2yAAAB', JSON.stringify( {
 				id: '123',
 				state: 'commenting'
-			}
-		] );
+			} ) );
+
+			expect( await repository.getAll( issueKey ) ).to.deep.equal( [
+				{
+					id: '123',
+					state: 'commenting'
+				}
+			] );
+		} );
+
+		it( 'should return an empty list of connected users when there are no users provided', async () => {
+			expect( await repository.getAll( issueKey ) ).to.deep.equal( [] );
+		} );
 	} );
 
-	it( 'should return an empty list of connected users when there are no users provided', async () => {
-		expect( await repository.getAll( issueKey ) ).to.deep.equal( [] );
+	describe( 'set()', () => {
+		it( 'should return a single connected user after insertion', async () => {
+			await repository.set( issueKey, 'IbVmSiD_LoULFK2yAAAB', {
+				id: '123',
+				state: 'commenting'
+			} );
+
+			expect( await repository.getAll( issueKey ) ).to.deep.equal( [
+				{
+					id: '123',
+					state: 'commenting'
+				}
+			] );
+		} );
+	} );
+
+	describe( 'delete()', () => {
+		it( 'should return a single connected user after insertion and an empty list of connected users after deletion', async () => {
+			await redisDriver.client.hset( issueKey, 'IbVmSiD_LoULFK2yAAAB', JSON.stringify( {
+				id: '123',
+				state: 'commenting'
+			} ) );
+
+			expect( await repository.getAll( issueKey ) ).to.deep.equal( [
+				{
+					id: '123',
+					state: 'commenting'
+				}
+			] );
+
+			await repository.delete( issueKey, 'IbVmSiD_LoULFK2yAAAB' );
+
+			expect( await repository.getAll( issueKey ) ).to.deep.equal( [] );
+		} );
 	} );
 } );
 
